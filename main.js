9f827618474ca4ed546cd475c0c1009c4e51553d
Maxtadont <tusaeff@gmail.com>
2020-12-15 21:55:48 +0400

    feat: create basic DOMobject model

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ import {FullscreenBtnMap} from "./assets/scripts/fullscreen.js";
 import {FullscreenBtnTable} from "./assets/scripts/fullscreen.js";
 import {FullscreenBtnChart} from "./assets/scripts/fullscreen.js";
 import {FullscreenBtnWrap} from "./assets/scripts/fullscreen.js";
+import {CountryGlobalData} from "./assets/scripts/CountryGlobalData.js";
 
 export const wrapBtn = new FullscreenBtnWrap('[data-wrap]');
 export const countryBtn = new FullscreenBtnCountry('[data-btn-country]', '[data-country]').showHideBtn().toggleFullscreen();
 export const mapBtn = new FullscreenBtnMap('[data-btn-map]', '[data-map]').showHideBtn().toggleFullscreen();
 export const tableBtn = new FullscreenBtnTable('[data-btn-table]', '[data-table]').showHideBtn().toggleFullscreen();
 export const chartBtn = new FullscreenBtnChart('[data-btn-chart]', '[data-chart]').showHideBtn().toggleFullscreen();
+
+export 
