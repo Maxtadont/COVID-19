20a15a67635618247bd8e0bbabcd7b5b9e6ea5b5
rzainullov <rashid.zainullov@gmail.com>
2020-12-19 12:13:35 +0400

    feat: implement map tabs for different indicators

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {FullscreenBtnCountry} from "./assets/scripts/fullscreen.js";
 import {FullscreenBtnMap} from "./assets/scripts/fullscreen.js";
 import {FullscreenBtnTable} from "./assets/scripts/fullscreen.js";
@@ -15,3 +16,31 @@ export const mapBtn = new FullscreenBtnMap("[data-btn-map]", "[data-map]").showH
 export const tableBtn = new FullscreenBtnTable("[data-btn-table]", "[data-table]").showHideBtn().toggleFullscreen();
 export const chartBtn = new FullscreenBtnChart("[data-btn-chart]", "[data-chart]").showHideBtn().toggleFullscreen();
 export const map = new InteractiveMap().createMapWrap().renderMap();
+=======
+import {FullscreenBtnCountry} from "./assets/scripts/fullscreen.js";
+import {FullscreenBtnMap} from "./assets/scripts/fullscreen.js";
+import {FullscreenBtnTable} from "./assets/scripts/fullscreen.js";
+import {FullscreenBtnChart} from "./assets/scripts/fullscreen.js";
+import {FullscreenBtnWrap} from "./assets/scripts/fullscreen.js";
+import {InteractiveMap} from "./assets/scripts/maps/maps.js";
+import {MapTab} from "./assets/scripts/maps/maps.js";
+import {mapsData} from "./assets/scripts/maps/maps.js";
+import {MapArea} from "./assets/scripts/maps/maps.js";
+
+
+
+
+export const wrapBtn = new FullscreenBtnWrap('[data-wrap]');
+export const countryBtn = new FullscreenBtnCountry('[data-btn-country]', '[data-country]').showHideBtn().toggleFullscreen();
+export const mapBtn = new FullscreenBtnMap('[data-btn-map]', '[data-map]').showHideBtn().toggleFullscreen();
+export const tableBtn = new FullscreenBtnTable('[data-btn-table]', '[data-table]').showHideBtn().toggleFullscreen();
+export const chartBtn = new FullscreenBtnChart('[data-btn-chart]', '[data-chart]').showHideBtn().toggleFullscreen();
+
+export const mapArea = new MapArea().createMapContainer()
+export const mapConfirmedCases = new InteractiveMap(mapsData.cases).createMapWrap().renderMap()
+export const tabConfirmedCases = new MapTab(mapsData.cases).createTab()
+export const mapConfirmedDeaths = new InteractiveMap(mapsData.deaths).createMapWrap().renderMap()
+export const tabConfirmedDeaths = new MapTab(mapsData.deaths).createTab()
+export const mapConfirmedRecovered = new InteractiveMap(mapsData.recovered).createMapWrap().renderMap()
+export const tabConfirmedRecovered = new MapTab(mapsData.recovered).createTab()
+>>>>>>> 19196ec... feat: implement map tabs for different indicators
